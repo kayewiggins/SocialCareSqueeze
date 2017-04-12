39b06294ae170e1c14873eadccfb39f5b1e6e3e1
kayewiggins <kaye.wiggins@city.ac.uk>
2017-04-12 08:36:09 +0100

    Add files via upload

---
+++ ageingsite.js
@@ -0,0 +1,671 @@
+$(function() {
+    var data = [
+
+ {
+   "A": 1,
+   "B": "Barking & Dagenham"
+ },
+ {
+   "A": 2,
+   "B": "Barnet"
+ },
+ {
+   "A": 3,
+   "B": "Barnsley"
+ },
+ {
+   "A": 4,
+   "B": "Bath & North East Somerset"
+ },
+ {
+   "A": 5,
+   "B": "Bexley"
+ },
+ {
+   "A": 6,
+   "B": "Birmingham"
+ },
+ {
+   "A": 7,
+   "B": "Blackburn with Darwen"
+ },
+ {
+   "A": 8,
+   "B": "Blackpool"
+ },
+ {
+   "A": 9,
+   "B": "Bolton"
+ },
+ {
+   "A": 10,
+   "B": "Bournemouth"
+ },
+ {
+   "A": 11,
+   "B": "Bracknell Forest"
+ },
+ {
+   "A": 12,
+   "B": "Bradford"
+ },
+ {
+   "A": 13,
+   "B": "Brent"
+ },
+ {
+   "A": 14,
+   "B": "Brighton & Hove"
+ },
+ {
+   "A": 15,
+   "B": "Bristol"
+ },
+ {
+   "A": 16,
+   "B": "Bromley"
+ },
+ {
+   "A": 17,
+   "B": "Buckinghamshire"
+ },
+ {
+   "A": 18,
+   "B": "Bury"
+ },
+ {
+   "A": 19,
+   "B": "Calderdale"
+ },
+ {
+   "A": 20,
+   "B": "Cambridgeshire"
+ },
+ {
+   "A": 21,
+   "B": "Camden"
+ },
+ {
+   "A": 22,
+   "B": "Cheshire East"
+ },
+ {
+   "A": 23,
+   "B": "Cheshire West and Chester"
+ },
+ {
+   "A": 24,
+   "B": "City of London"
+ },
+ {
+   "A": 25,
+   "B": "Coventry"
+ },
+ {
+   "A": 26,
+   "B": "Croydon"
+ },
+ {
+   "A": 27,
+   "B": "Cumbria"
+ },
+ {
+   "A": 28,
+   "B": "Darlington"
+ },
+ {
+   "A": 29,
+   "B": "Derby"
+ },
+ {
+   "A": 30,
+   "B": "Derbyshire"
+ },
+ {
+   "A": 31,
+   "B": "Devon"
+ },
+ {
+   "A": 32,
+   "B": "Doncaster"
+ },
+ {
+   "A": 33,
+   "B": "Dorset"
+ },
+ {
+   "A": 34,
+   "B": "Dudley"
+ },
+ {
+   "A": 35,
+   "B": "Ealing"
+ },
+ {
+   "A": 36,
+   "B": "East Riding of Yorkshire"
+ },
+ {
+   "A": 37,
+   "B": "East Sussex"
+ },
+ {
+   "A": 38,
+   "B": "Enfield"
+ },
+ {
+   "A": 39,
+   "B": "Essex"
+ },
+ {
+   "A": 40,
+   "B": "Gateshead"
+ },
+ {
+   "A": 41,
+   "B": "Gloucestershire"
+ },
+ {
+   "A": 42,
+   "B": "Greenwich"
+ },
+ {
+   "A": 43,
+   "B": "Hackney"
+ },
+ {
+   "A": 44,
+   "B": "Halton"
+ },
+ {
+   "A": 45,
+   "B": "Hammersmith & Fulham"
+ },
+ {
+   "A": 46,
+   "B": "Hampshire"
+ },
+ {
+   "A": 47,
+   "B": "Haringey"
+ },
+ {
+   "A": 48,
+   "B": "Harrow"
+ },
+ {
+   "A": 49,
+   "B": "Hartlepool"
+ },
+ {
+   "A": 50,
+   "B": "Havering"
+ },
+ {
+   "A": 51,
+   "B": "Herefordshire"
+ },
+ {
+   "A": 52,
+   "B": "Hertfordshire"
+ },
+ {
+   "A": 53,
+   "B": "Hillingdon"
+ },
+ {
+   "A": 54,
+   "B": "Hounslow"
+ },
+ {
+   "A": 55,
+   "B": "Isle of Wight"
+ },
+ {
+   "A": 56,
+   "B": "Isles of Scilly"
+ },
+ {
+   "A": 57,
+   "B": "Islington"
+ },
+ {
+   "A": 58,
+   "B": "Kensington & Chelsea"
+ },
+ {
+   "A": 59,
+   "B": "Kent"
+ },
+ {
+   "A": 60,
+   "B": "Kingston upon Hull"
+ },
+ {
+   "A": 61,
+   "B": "Kingston upon Thames"
+ },
+ {
+   "A": 62,
+   "B": "Kirklees"
+ },
+ {
+   "A": 63,
+   "B": "Knowsley"
+ },
+ {
+   "A": 64,
+   "B": "Lambeth"
+ },
+ {
+   "A": 65,
+   "B": "Lancashire"
+ },
+ {
+   "A": 66,
+   "B": "Leeds"
+ },
+ {
+   "A": 67,
+   "B": "Leicester City"
+ },
+ {
+   "A": 68,
+   "B": "Leicestershire"
+ },
+ {
+   "A": 69,
+   "B": "Lewisham"
+ },
+ {
+   "A": 70,
+   "B": "Lincolnshire"
+ },
+ {
+   "A": 71,
+   "B": "Liverpool"
+ },
+ {
+   "A": 72,
+   "B": "Luton"
+ },
+ {
+   "A": 73,
+   "B": "Manchester"
+ },
+ {
+   "A": 74,
+   "B": "Merton"
+ },
+ {
+   "A": 75,
+   "B": "Middlesbrough"
+ },
+ {
+   "A": 76,
+   "B": "Milton Keynes"
+ },
+ {
+   "A": 77,
+   "B": "Newcastle upon Tyne"
+ },
+ {
+   "A": 78,
+   "B": "Newham"
+ },
+ {
+   "A": 79,
+   "B": "Norfolk"
+ },
+ {
+   "A": 80,
+   "B": "North East Lincolnshire"
+ },
+ {
+   "A": 81,
+   "B": "North Lincolnshire"
+ },
+ {
+   "A": 82,
+   "B": "North Somerset"
+ },
+ {
+   "A": 83,
+   "B": "North Tyneside"
+ },
+ {
+   "A": 84,
+   "B": "North Yorkshire"
+ },
+ {
+   "A": 85,
+   "B": "Northamptonshire"
+ },
+ {
+   "A": 86,
+   "B": "Nottingham"
+ },
+ {
+   "A": 87,
+   "B": "Nottinghamshire"
+ },
+ {
+   "A": 88,
+   "B": "Oldham"
+ },
+ {
+   "A": 89,
+   "B": "Oxfordshire"
+ },
+ {
+   "A": 90,
+   "B": "Peterborough"
+ },
+ {
+   "A": 91,
+   "B": "Plymouth"
+ },
+ {
+   "A": 92,
+   "B": "Poole"
+ },
+ {
+   "A": 93,
+   "B": "Portsmouth"
+ },
+ {
+   "A": 94,
+   "B": "Reading"
+ },
+ {
+   "A": 95,
+   "B": "Redbridge"
+ },
+ {
+   "A": 96,
+   "B": "Redcar & Cleveland"
+ },
+ {
+   "A": 97,
+   "B": "Richmond upon Thames"
+ },
+ {
+   "A": 98,
+   "B": "Rochdale"
+ },
+ {
+   "A": 99,
+   "B": "Rotherham"
+ },
+ {
+   "A": 100,
+   "B": "Rutland"
+ },
+ {
+   "A": 101,
+   "B": "Salford"
+ },
+ {
+   "A": 102,
+   "B": "Sandwell"
+ },
+ {
+   "A": 103,
+   "B": "Sefton"
+ },
+ {
+   "A": 104,
+   "B": "Sheffield"
+ },
+ {
+   "A": 105,
+   "B": "Slough"
+ },
+ {
+   "A": 106,
+   "B": "Solihull"
+ },
+ {
+   "A": 107,
+   "B": "Somerset"
+ },
+ {
+   "A": 108,
+   "B": "South Gloucestershire"
+ },
+ {
+   "A": 109,
+   "B": "South Tyneside"
+ },
+ {
+   "A": 110,
+   "B": "Southampton"
+ },
+ {
+   "A": 111,
+   "B": "Southend-on-Sea"
+ },
+ {
+   "A": 112,
+   "B": "Southwark"
+ },
+ {
+   "A": 113,
+   "B": "St Helens"
+ },
+ {
+   "A": 114,
+   "B": "Staffordshire"
+ },
+ {
+   "A": 115,
+   "B": "Stockport"
+ },
+ {
+   "A": 116,
+   "B": "Stockton-on-Tees"
+ },
+ {
+   "A": 117,
+   "B": "Stoke-on-Trent"
+ },
+ {
+   "A": 118,
+   "B": "Suffolk"
+ },
+ {
+   "A": 119,
+   "B": "Sunderland"
+ },
+ {
+   "A": 120,
+   "B": "Surrey"
+ },
+ {
+   "A": 121,
+   "B": "Sutton"
+ },
+ {
+   "A": 122,
+   "B": "Swindon"
+ },
+ {
+   "A": 123,
+   "B": "Tameside"
+ },
+ {
+   "A": 124,
+   "B": "Telford and Wrekin"
+ },
+ {
+   "A": 125,
+   "B": "Thurrock"
+ },
+ {
+   "A": 126,
+   "B": "Torbay"
+ },
+ {
+   "A": 127,
+   "B": "Tower Hamlets"
+ },
+ {
+   "A": 128,
+   "B": "Trafford"
+ },
+ {
+   "A": 129,
+   "B": "Wakefield"
+ },
+ {
+   "A": 130,
+   "B": "Walsall"
+ },
+ {
+   "A": 131,
+   "B": "Waltham Forest"
+ },
+ {
+   "A": 132,
+   "B": "Wandsworth"
+ },
+ {
+   "A": 133,
+   "B": "Warrington"
+ },
+ {
+   "A": 134,
+   "B": "Warwickshire"
+ },
+ {
+   "A": 135,
+   "B": "West Berkshire"
+ },
+ {
+   "A": 136,
+   "B": "West Sussex"
+ },
+ {
+   "A": 137,
+   "B": "Westminster"
+ },
+ {
+   "A": 138,
+   "B": "Wigan"
+ },
+ {
+   "A": 139,
+   "B": "Windsor & Maidenhead"
+ },
+ {
+   "A": 140,
+   "B": "Wirral"
+ },
+ {
+   "A": 141,
+   "B": "Wokingham"
+ },
+ {
+   "A": 142,
+   "B": "Wolverhampton"
+ },
+ {
+   "A": 143,
+   "B": "Worcestershire"
+ },
+ {
+   "A": 144,
+   "B": "York"
+ }
+];
+
+    $.each(data, function(i, option) {
+        $('#sel').append($('<option/>').attr("value", option.A).text(option.B));
+    });
+})
+
+Highcharts.chart('container', {
+    chart: {
+        zoomType: 'xy'
+    },
+    title: {
+        text: 'The Social Care Squeeze: How spending on elderly care has fallen as the number of pensioners has grown'
+    },
+    subtitle: {
+        text: 'Source: Department for Communities and Local Government, Office for National Statistics'
+    },
+    xAxis: [{
+        categories: ['2008', '2009', '2010', '2011', '2012', '2013',
+            '2014', '2015'],
+        crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+
+            min: 7000000,
+            max: 9000000,
+            labels: {
+            format: '{value}',
+            style: {
+                color: Highcharts.getOptions().colors[2]
+            }
+        },
+        title: {
+            text: 'Amount spent (thousands)',
+            style: {
+                color: Highcharts.getOptions().colors[2]
+            }
+        },
+
+        opposite: true
+
+    }, { // Secondary yAxis
+      min: 7000000,
+      max: 9000000,
+        title: {
+            text: 'Number of people aged 65+',
+            style: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        },
+        labels: {
+            format: '{value}',
+            style: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        }
+
+    }, { 
+    }],
+    tooltip: {
+        shared: false
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'left',
+        x: 80,
+        verticalAlign: 'top',
+        y: 55,
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    series: [{
+        name: 'Number of people aged 65+',
+        type: 'column',
+        yAxis: 1,
+        data: [7864832,7995188,8137620,8267408,8569016,8795331,9007307,9167261],
+        tooltip: {
+            valueSuffix: ''
+        }
+
+    }, {
+        name: 'Amount spent',
+        type: 'spline',
+        yAxis: 0,
+        data: [8799748,9368214,9408677,9255673,9282415,9316532,7806011,7614605.931],
+        tooltip: {
+            valueSuffix: '(thousands, GBP)'
+        }
+    }]
+});
+
+
